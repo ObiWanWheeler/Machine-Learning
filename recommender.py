c9dba8013c1599fb8e989446bfae485314a250d2
ObiWanWheeler <charliestinhorto@gmail.com>
2021-07-19 18:47:06 +0100

    make sure that df is deep copied

--- recommender.py
+++ recommender.py
@@ -1,23 +1,9 @@
-import numpy as np
-from pandas.core.arrays.categorical import contains
-from pandas.core.frame import DataFrame
-import scipy
 import pandas as pd
-import math
-import random
-import sklearn
-from scipy.sparse import csr_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse.linalg import svds
-from sklearn.preprocessing import MinMaxScaler
 import re
-from popularity_recommender import PopularityRecommender
-from utils import filter_by_query, calculate_tfidf_matrix
-from profile_builder import ProfileBuilder
 from content_recommender import ContentRecommender
-
+from popularity_recommender import PopularityRecommender
+from profile_builder import ProfileBuilder
+from utils import calculate_tfidf_matrix, filter_by_query
 
 # data sets
 anime = pd.read_csv("./data/anime/anime.csv", low_memory=False)
@@ -29,7 +15,7 @@ anime['type'].fillna('', inplace=True)
 anime['name'].fillna('', inplace=True)
 
 # abridging ratings so the ol' laptop can actually run it
-ratings = ratings[ratings['user_id'] <= 500]
+ratings = ratings[ratings['user_id'] <= 1000]
 
 # content stuff
 item_ids = anime['anime_id'].tolist()
@@ -39,17 +25,18 @@ watched_ratings = ratings.loc[ratings['rating'] != -1]
 
 
 # pr = PopularityRecommender()
-# print(pr.give_recommendation(anime, 10))
+# print(f'Most popular anime overall:\n {pr.give_recommendation(anime, 10)}')
 
 # action_or_adventure_anime = filter_by_query('genre', lambda x: bool(
 #     re.search(r'Action|Adventure', x, re.IGNORECASE)), anime)
-# print(action_or_adventure_anime.head(10))
+# print(f'Most popular Action / Adventure anime:\n {pr.give_recommendation(action_or_adventure_anime, 10)}')
 
-pb = ProfileBuilder(anime, item_ids, watched_ratings, tfidf_matrix)
-user_profiles = pb.build_all_user_profiles()
-print(pd.DataFrame(sorted(zip(tfidf_feature_names,
-                              user_profiles[2].flatten().tolist()), key=lambda x: -x[1])[:20],
-                   columns=['token', 'relevance']))
-cr = ContentRecommender()
-recs = cr.give_recommendation(user_profiles[2], tfidf_matrix, item_ids, anime)
-print(recs)
+# pb = ProfileBuilder(anime, item_ids, watched_ratings, tfidf_matrix)
+# user_profiles = pb.build_all_user_profiles()
+# print(pd.DataFrame(sorted(zip(tfidf_feature_names,
+#                               user_profiles[1].flatten().tolist()), key=lambda x: -x[1])[:20],
+#                    columns=['token', 'relevance']))
+# print(len(user_profiles))
+# cr = ContentRecommender()
+# recs = cr.give_recommendation(user_profiles[1], tfidf_matrix, item_ids, anime)
+# print(recs)
