c25e26a3a3ed6bd584141095c0119a52ed28f7d2
ObiWanWheeler <charliestinhorto@gmail.com>
2021-07-21 10:51:55 +0100

    implement collaborative filtering

---
+++ collaborative_recommender.py
@@ -0,0 +1,4 @@
+class CollabRecommender:
+
+    def __init__(self, ratings_df) -> None:
+        self.ratings_df = ratings_df
--- recommender.py
+++ recommender.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import re
+import numpy as np
+from pandas.core.frame import DataFrame
 from content_recommender import ContentRecommender
 from popularity_recommender import PopularityRecommender
 from profile_builder import ProfileBuilder
 from utils import calculate_tfidf_matrix, filter_by_query
+from scipy.sparse.linalg import svds
 
 # data sets
 anime = pd.read_csv("./data/anime/anime.csv", low_memory=False)
@@ -23,6 +26,33 @@ tfidf_feature_names, tfidf_matrix = calculate_tfidf_matrix(anime)
 
 watched_ratings = ratings.loc[ratings['rating'] != -1]
 
+# collaborative stuff
+user_ratings = watched_ratings.pivot(
+    index='user_id', columns='anime_id', values='rating').fillna(0)
+user_ratings_matrix = user_ratings.to_numpy()
+user_ratings_means = np.mean(user_ratings_matrix, axis=1)
+user_ratings_demeaned = user_ratings_matrix - user_ratings_means.reshape(-1, 1)
+
+u, sigma, vt = svds(user_ratings_demeaned, k=75)
+sigma = np.diag(sigma)
+predictions_matrix = np.dot(np.dot(u, sigma), vt) + \
+    user_ratings_means.reshape(-1, 1)
+predictions_df = DataFrame(predictions_matrix, columns=user_ratings.columns)
+
+
+def give_recommendations(user_id, items_df=None, topn=10):
+    user_predictions = predictions_df.iloc[user_id].sort_values(
+        ascending=False).reset_index().rename(columns={user_id: 'relevance'})
+
+    user_ratings = watched_ratings[watched_ratings['user_id'] == user_id]
+
+    recommendations = user_predictions[~user_predictions['anime_id'].isin(
+        user_ratings['anime_id'])].sort_values(by='relevance', ascending=False).head(topn)
+
+    if items_df is not None:
+        recommendations = recommendations.merge(items_df, how='left', left_on='anime_id', right_on='anime_id')
+
+    return recommendations
 
 # pr = PopularityRecommender()
 # print(f'Most popular anime overall:\n {pr.give_recommendation(anime, 10)}')
@@ -40,3 +70,5 @@ watched_ratings = ratings.loc[ratings['rating'] != -1]
 # cr = ContentRecommender()
 # recs = cr.give_recommendation(user_profiles[1], tfidf_matrix, item_ids, 'anime_id', anime)
 # print(recs)
+
+print(give_recommendations(0, anime))
