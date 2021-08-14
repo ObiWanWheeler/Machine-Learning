fc34ba8729abfe5e104fcfc270c8dd8fb956c85f
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-08-14 09:59:17 +0100

    add walrus operator

--- src/main.py
+++ src/main.py
@@ -55,11 +55,9 @@ def index():
 
 
 def recommender_route(user_id, recommendation_func):
-    topn = request.args.get('topn')
-    if topn is None:
+    if (topn := request.args.get('topn')) is None:
         topn = 10
-    verbose = request.args.get('verbose')
-    if verbose is None:
+    if (verbose := request.args.get('verbose')) is None:
         verbose = False
 
     items_to_ignore = list(
